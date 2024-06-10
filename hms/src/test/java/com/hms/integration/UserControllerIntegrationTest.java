b25bcc1625bc153941c36629e04049ba15b025f4
ramar-012 <ramarj2001@outlook.com>
2024-06-10 14:38:00 +0530

    Test files getting updated.

--- hms/src/test/java/com/hms/integration/UserControllerIntegrationTest.java
+++ hms/src/test/java/com/hms/integration/UserControllerIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
 import org.springframework.test.context.ActiveProfiles;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Objects;
 
@@ -24,29 +25,53 @@ public class UserControllerIntegrationTest {
     private int port;
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private UserRepository userRepository;
 
     @Autowired
-    private UserRepository userRepository;
+    private TestRestTemplate restTemplate;
 
     private String baseURL;
 
     @BeforeEach
-    public void setUp(){
+    @Transactional
+    public void setUp() {
         baseURL = "http://localhost:" + port + "/api/users";
         userRepository.deleteAll();
 
         //users for testing
-        userRepository.save(new User(1L, "user1", "user1@example.com", "password1", true, "ROLE_USER", "male"));
-        userRepository.save(new User(2L, "user2", "user2@example.com", "password2", true, "ROLE_USER", "female"));
+        userRepository.saveAndFlush(new User(5, "user5", "user5@example.com", "password1", true, "ROLE_USER", "male"));
+        userRepository.saveAndFlush(new User(6, "staff6", "staff6@example.com", "password1", true, "ROLE_STAFF", "female"));
     }
 
     @Test
-    public void testGetAllUsers(){
-        ResponseEntity<User [] > response = restTemplate.getForEntity(baseURL, User[].class);
+    public void testGetAllUsers() {
+        try {
+            ResponseEntity<User[]> response = restTemplate.getForEntity(baseURL, User[].class);
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+            List<User> users = List.of(Objects.requireNonNull(response.getBody()));
+            assertNotNull(users);
+            assertEquals(2, users.size());
+            System.out.println("CIT Get all users test passed!");
+        } catch (Exception e) {
+            System.out.println("CIT Get all users: Test failed " + e);
+            // Optionally, rethrow the exception to ensure the test fails
+            throw e;
+        }
+    }
+
+
+    @Test
+    public void testGetUserById(){
+        ResponseEntity<User> response = restTemplate.getForEntity(baseURL+ "/5", User.class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        List<User> users = List.of(Objects.requireNonNull(response.getBody()));
-        assertNotNull(users);
-        assertEquals(2, users.size());
+        try{
+            User user = response.getBody();
+            assertNotNull(user);
+            assertEquals("user5", user.getUsername());
+            System.out.println("CIT Get user by ID test passed!\n" + user.toString());
+        } catch (Exception e){
+            System.out.println("CIT Get all user by ID test failed " + e);
+        }
+
     }
 }
